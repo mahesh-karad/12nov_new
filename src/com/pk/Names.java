9b574f4fbbac1ee2d185e392ae7ee3323e6429d7
DELL <DELL@DESKTOP-TONBJ34>
2020-11-30 15:54:33 +0530

    Added name function

--- src/com/pk/Names.java
+++ src/com/pk/Names.java
@@ -1,8 +1,17 @@
 package com.pk;
 
-public class Names {
 
-	public static void main(String[] args) {
+
+public class Names 
+{
+	
+	void Api864()
+	{
+		System.out.println("-----DEVESH VAIDYA-----");
+	}
+
+	public static void main(String[] args) 
+	{
 	System.out.println("this is mahesh karad");
     System.out.println("This is Harshaan Arora");
 	}
