41f31b5e0deeb5e9044fb7683e21363f103e8893
DELL <DELL@DESKTOP-J7R6PB3>
2020-11-30 16:01:23 +0530

    Version Shweta

--- src/com/pk/Names.java
+++ src/com/pk/Names.java
@@ -5,6 +5,11 @@ public class Names {
 	public static void main(String[] args) {
 	System.out.println("this is mahesh karad");
     System.out.println("This is Harshaan Arora");
+    
 	}
-
+public void Shweta()
+{
+	
+	System.out.println("This is Shweta Sinha");
+}
 }
