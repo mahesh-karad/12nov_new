4d609751a6d95cb5dc6161fd3b1e88c7df57c422
DELL <DELL@DESKTOP-J7R6PB3>
2020-11-30 16:19:40 +0530

    Shweta class

---
+++ src/com/pk/Shweta.java
@@ -0,0 +1,14 @@
+package com.pk;
+
+public class Shweta {
+        int empid;
+        public Shweta()
+        {
+        	empid=857;
+        }
+	public static void main(String[] args) {
+		
+                  System.out.println("This is me Shweta Sinha");
+	}
+
+}
