ea5649f1bbaad132a3576d22f4cee60e2000273f
DELL <DELL@DESKTOP-EMVDL34>
2020-11-30 16:18:29 +0530

    Commit by Janhavi

---
+++ src/com/pk/API856.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+public class API856 {
+	public void janhavi() {
+		System.out.println("This is Janhavi Pradhan");
+	}
+	public static void main(String[] args) {
+		API856 jsp = new API856();
+		jsp.janhavi();
+
+	}
+
+}
